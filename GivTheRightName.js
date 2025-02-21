d7eb82bd82eba1faca352bdf0bc8acc354567407
qwerty213465432 <157486853+qwerty213465432@users.noreply.github.com>
2025-02-21 16:29:58 +0500

    Add files via upload

---
+++ GivTheRightName.js
@@ -0,0 +1,5 @@
+
+
+let ourPlanetName = "Earth";//The variable for our planet
+
+let currentUserName = "John";//The name of the current visitor
---
+++ UpCaseConst.js
@@ -0,0 +1,3 @@
+const BIRTHDAY = '18.04.1982';  //We generally use upper case for constants that are “hard-coded”.
+
+const AGE = someCode(BIRTHDAY);// Or, in other words, when the value is known prior to execution and directly written into the code.
